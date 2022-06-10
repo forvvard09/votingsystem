e8f556e1f0c18f058ec3054e4c7b119600636137
Sergei Poddubniak <forvvard09@gmail.com>
2022-06-10 13:14:26 +0300

    Add loging for app, slf4j + logback

--- src/main/java/com/forvvard09/votingsystem/web/RestaurantServlet.java
+++ src/main/java/com/forvvard09/votingsystem/web/RestaurantServlet.java
@@ -1,17 +1,28 @@
 package com.forvvard09.votingsystem.web;
 
+
+
+
+
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+
 public class RestaurantServlet extends HttpServlet {
+    private static final Logger log = getLogger(RestaurantServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //request.getRequestDispatcher("/index.html").forward(request, response);
-        request.getRequestDispatcher("restaurants.jsp").forward(request, response);
-        //response.sendRedirect("restaurants.jsp");
+        log.debug("Тестирование логирования. Пришли в сервлет. Редирект на список ресторанов");
+        log.info("Redirect to restaurants.jsp");
+        //request.getRequestDispatcher("restaurants.jsp").forward(request, response);
+        response.sendRedirect("restaurants.jsp");
     }
 }
