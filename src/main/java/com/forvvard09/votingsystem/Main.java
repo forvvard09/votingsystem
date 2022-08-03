8306d0afc9bd450931b212108d8fa213ffc8ed38
Sergei Poddubniak <forvvard09@gmail.com>
2022-08-04 01:56:18 +0300

    Add loging for app, slf4j + logback

--- src/main/java/com/forvvard09/votingsystem/Main.java
+++ src/main/java/com/forvvard09/votingsystem/Main.java
@@ -1,23 +1,18 @@
 package com.forvvard09.votingsystem;
 
-import com.forvvard09.votingsystem.model.Menu;
-import com.forvvard09.votingsystem.model.Restaurant;
+import com.forvvard09.votingsystem.repository.InMemoryRestaurants;
+import com.forvvard09.votingsystem.repository.RestaurantsRepository;
 
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Menu> menuList = Arrays.asList(
-                new Menu("Твороженная запеканка", 7500),
-                new Menu("Борщ", 12600),
-                new Menu("Омлет", 8900)
-        );
+        RestaurantsRepository repository;
+        repository = new InMemoryRestaurants();
 
-        Restaurant restaurant = new Restaurant(99,"Теплица", LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), menuList);
-        restaurant.setMenuItem(new Menu("Салат Цезарь", 14300));
+        repository.getAll().forEach(System.out::println);
+        //System.out.println(repository.getAll());
+
     }
+
 }
